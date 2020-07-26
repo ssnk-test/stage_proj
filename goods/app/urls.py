391974f0c8a11a858fb8511342524f6fbad14ecc
Kozlov Sergei <ssnk@yandex.ru>
2020-07-27 00:26:10 +0400

    Добавлен swagger /swagger/

--- goods/app/urls.py
+++ goods/app/urls.py
@@ -1,12 +1,25 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 from .views import StickerList, StickerDetail,TagList
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(title="API",default_version="None",),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    url(r"^swagger/$",schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",),
     path('items/', StickerList.as_view()),
     path('items/<int:pk>/', StickerDetail.as_view()),
     path('tags/', TagList.as_view()),
+
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- goods/config/settings.py
+++ goods/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "drf_yasg",
     'app',
 ]
 
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema'}
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
